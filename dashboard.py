51349809692b8d8b6581d29ebea9994a4ff43f0f
Kauan Pontes <154468574+kauanpscode@users.noreply.github.com>
2025-03-17 13:58:33 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -103,7 +103,7 @@ def load_data():
     
     df_seller['data'] = df_seller['DATA DE TRATAMENTO'].dt.date
     df_seller['data'] = pd.to_datetime(df_seller['data'], errors='coerce')
-    df_seller['mes_seller'] = df_seller['mês']
+    df_seller['mes_seller'] = df_seller['data'].dt.to_period('M')
 
     return df, df_seller
 
